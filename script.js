2c41e459e1a9b2e87f811480a76528b23dd5ee3b
Arvidian64 <[arvid.sthl@yahoo.com]>
2024-08-15 21:18:58 +0200

    added the six relevant math functions. More to come

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function add(x,y) {
+    z = x + y;
+    return z;
+}
+
+function subtract(x,y) {
+    z = x - y;
+    return z;
+}
+
+function multiply(x,y) {
+    z = x * y;
+    return z;
+}
+
+function divide(x,y) {
+    z = x / y;
+    return z;
+}
+function toThePowerOf(x,y) {
+    z = x **y;
+    return z;
+}
+
+function rootOf(x) {
+    z = math.sqrt(x);
+    return z;
+}
